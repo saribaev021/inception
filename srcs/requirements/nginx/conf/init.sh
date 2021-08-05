338cf62477f73f7aa4b72a13a8757002a7faa2d0
Kanat Saribaew <ndreadno@mi-d3.msk.21-school.ru>
2021-08-05 20:49:49 +0300

    1

--- srcs/requirements/nginx/conf/init.sh
+++ srcs/requirements/nginx/conf/init.sh
@@ -1,5 +1,5 @@
-cp -r /wordpress/* /var/www/wordpress/
 # mv /wp-config.php /var/www/wordpress
 chown -R ndreadno:ndreadno /var/www/*
 chmod -R 755 /var/www/*
 nginx -g "daemon off;"
+#/var/www/wordpress
--- srcs/requirements/wordpress/conf/init.sh
+++ srcs/requirements/wordpress/conf/init.sh
@@ -4,4 +4,11 @@ chown -R www-data:www-data /var/www/*
 chmod -R 777 /var/www/*
 # chown -R www-data:www-data /run/php/*
 # chmod -R 755 /run/php/*
+
+cd /var/www/wordpress
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod 777 wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+# wp --allow-root db check
+
 /usr/sbin/php-fpm7.3 -F 
