61e3971ebf7184ebd3af53457efc5c93661e13e5
Kanat Saribaew <ndreadno@mi-d3.msk.21-school.ru>
2021-08-07 19:11:37 +0300

    1

--- srcs/requirements/nginx/conf/init.sh
+++ srcs/requirements/nginx/conf/init.sh
@@ -1,5 +1,5 @@
 # mv /wp-config.php /var/www/wordpress
-chown -R ndreadno:ndreadno /var/www/*
+chown -R ${NGINX_USER}:${NGINX_USER} /var/www/*
 chmod -R 755 /var/www/*
 nginx -g "daemon off;"
 #/var/www/wordpress
