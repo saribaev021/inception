f0fcb298ab09f139a03bc7f4bbb47d71e6ecd60f
kanat <saribaev021@gmail.com>
2021-08-07 17:08:41 +0300

    1

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -2,7 +2,9 @@
 
 
 service mysql start
-mysql -u root mysql < init_db.sql;
+mysql -u root < set_root_password.sql
+mysql -u root -p${MYSQL_ROOT_PASSWORD} mysql  < init_db.sql;
+mysql -u root -p${MYSQl_ROOT_PASSWORD} wordpress < wordpress.sql;
 service mysql stop 
 /usr/bin/mysqld_safe ;
 
---
+++ srcs/requirements/mariadb/conf/set_root_password.sql
@@ -0,0 +1,2 @@
+ALTER USER root@localhost IDENTIFIED VIA mysql_native_password;
+SET PASSWORD = PASSWORD('mypass');
